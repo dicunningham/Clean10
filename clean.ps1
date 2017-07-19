2cf1926cbbe3c546ad966a0d2071755970d7cf46
higgins00 <danie.higgins@gmail.com>
2017-07-19 08:54:22 -0500

    Added descriptions
    Added descriptions of built-in applications for easier customization

--- clean.ps1
+++ clean.ps1
@@ -1,7 +1,7 @@
 ﻿# windows_StartClean.ps1
 
 ###############################
-# Run as Admin
+# Run as PowerShell Admin
 ###############################
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
 
@@ -15,31 +15,87 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 # Be gone, heathens!
 ###############################
 
+
+# 3D Builder and View 3D
 Get-AppxPackage -AllUsers *3d* | Remove-AppxPackage
+
+# Alarms and Clock Apps
 Get-AppxPackage -AllUsers *alarms* | Remove-AppxPackage
+
+# App Connector (Azure, MSN-based applications; live tiles.)
 Get-AppxPackage -AllUsers *appconnector* | Remove-AppxPackage
+
+# App Installer - (Web-based applications)
 Get-AppxPackage -AllUsers *appinstaller* | Remove-AppxPackage
+
+# Bing Money, News, Sports, Weather
 Get-AppxPackage -AllUsers *bing* | Remove-AppxPackage
+
+# Candy Crush
 Get-AppxPackage -AllUsers *CandyCrush* | Remove-AppxPackage
+
+# Calculator App
+#Get-AppxPackage -AllUsers *calculator* | Remove-AppxPackage
+
+# Calendar and Mail App
 Get-AppxPackage -AllUsers *communicationsapps* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *connectivitystore* | Remove-AppxPackage
+
+# Feedback Hub 
 Get-AppxPackage -AllUsers *feedback* | Remove-AppxPackage
+
+# Get Started or Tips
 Get-AppxPackage -AllUsers *GetStarted* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *maps* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Messaging* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *paint* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *officehub* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *oneconnect* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *onenote* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *phone* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *skypeapp* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *soundrecorder* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *sticky* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *sway* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *wallet* | Remove-AppxPackage
+
+# Groove Music and Movies & TV
 Get-AppxPackage -AllUsers *Zune* | Remove-AppxPackage
 
-#Get-AppxPackage -AllUsers *calculator* | Remove-AppxPackage
-#Get-AppxPackage -AllUsers *solitaire* | Remove-AppxPackage
-#Get-AppxPackage -AllUsers *xbox* | Remove-AppxPackage
+# Maps App
+Get-AppxPackage -AllUsers *maps* | Remove-AppxPackage
+
+# Messaging and Skype Video apps
+Get-AppxPackage -AllUsers *Messaging* | Remove-AppxPackage
+
+# Paid Wi-Fi & Cellular
+Get-AppxPackage -AllUsers *oneconnect* | Remove-AppxPackage
+
+# Paint 3D App (old paint still usable)
+Get-AppxPackage -AllUsers *paint* | Remove-AppxPackage
+
+# People / Contacts App
+Get-AppxPackage -AllUsers *people* | Remove-AppxPackage
+
+# Phone and Phone Companion apps
+Get-AppxPackage -AllUsers *phone* | Remove-AppxPackage
+
+# Photos App
+Get-AppxPackage -Allusers *photos* | Remove-AppxPackage
+
+# Preinstalled Get Office
+Get-AppxPackage -AllUsers *officehub* | Remove-AppxPackage
+
+# OneNote App (does not affect desktop version)
+Get-AppxPackage -AllUsers *onenote* | Remove-AppxPackage
+
+# Skype App (not desktop version)
+Get-AppxPackage -AllUsers *skypeapp* | Remove-AppxPackage
+
+# Voice Recorder
+Get-AppxPackage -AllUsers *soundrecorder* | Remove-AppxPackage
+
+# Solitaire Collection
+Get-AppxPackage -AllUsers *solitaire* | Remove-AppxPackage
+
+# Sticky Notes App
+Get-AppxPackage -AllUsers *sticky* | Remove-AppxPackage
+
+# Sway
+Get-AppxPackage -AllUsers *sway* | Remove-AppxPackage
+
+# Microsoft Wallet
+Get-AppxPackage -AllUsers *wallet* | Remove-AppxPackage
+
+# Microsoft Wi-Fi (does not affect network abilites)
+Get-AppxPackage -AllUsers *connectivitystore* | Remove-AppxPackage
+
+# Xbox 
+Get-AppxPackage -AllUsers *xbox* | Remove-AppxPackage
